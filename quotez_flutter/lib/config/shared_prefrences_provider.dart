f6c280309e1ec42dcff7328a0c9ea0696e4d74f3
mohamed abderraouf douadi <me.is.intik@gmail.com>
2024-07-14 12:51:53 +0100

    changed provider to generated

--- quotez_flutter/lib/config/shared_prefrences_provider.dart
+++ quotez_flutter/lib/config/shared_prefrences_provider.dart
@@ -1,6 +1,9 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+part 'shared_prefrences_provider.g.dart';
+
+@riverpod
+SharedPreferences sharedPreferences(SharedPreferencesRef ref) {
   throw UnimplementedError();
-});
+}
